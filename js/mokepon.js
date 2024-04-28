8654a37990902ef9a5cdb183b094fb5138f96b7a
Marco Jorge <marcojorge99@gmail.com>
2024-04-28 12:49:28 -0500

    creamos el archivo js junto con el comando script en el html

---
+++ js/mokepon.js
@@ -0,0 +1,7 @@
+function seleccionarMascotaJugador(){
+    alert("Seleccionas tu mascota")
+}
+
+let botonMascotaJugador = document.getElementById("boton-mascota")
+botonMascotaJugador.addEventListener("click",seleccionarMascotaJugador)
+
