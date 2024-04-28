ebbebbe79b653059bb619a9ca770696f023dd3c5
Marco Jorge <marcojorge99@gmail.com>
2024-04-28 13:01:12 -0500

    añadimos alertas cuando el cliente elige una mascota y usamos una función window load para que se lean los botones luego de cargar la página

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,7 +1,26 @@
-function seleccionarMascotaJugador(){
-    alert("Seleccionas tu mascota")
+function iniciarJuego(){
+    let botonMascotaJugador = document.getElementById("boton-mascota")
+    botonMascotaJugador.addEventListener("click",seleccionarMascotaJugador)
 }
 
-let botonMascotaJugador = document.getElementById("boton-mascota")
-botonMascotaJugador.addEventListener("click",seleccionarMascotaJugador)
+function seleccionarMascotaJugador(){
 
+    let inputHipodoge = document.getElementById('hipodoge')
+    let inputCapipepo = document.getElementById('capipepo')
+    let inputRatigueya = document.getElementById('ratigueya')
+
+    if (inputHipodoge.checked) {
+        alert("Seleccionaste Hipodoge")
+    } else if (inputCapipepo.checked) {
+        alert("Seleccionaste Capipepo")
+    } else if (inputRatigueya.checked) {
+        alert("Seleccionaste Ratigueya")
+    } else {
+        alert("Selecciona una mascota")
+    }
+}
+
+
+
+
+window.addEventListener("load", iniciarJuego)
