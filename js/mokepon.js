1cc619826fb3b97fa382975ca1c628ac51acf140
Marco Jorge <marcojorge99@gmail.com>
2024-05-04 11:48:28 -0500

    creamos la función de colisiones que revisará si existe alguna colisión. Además, devolvemos el nombre de la mascota enemiga con la que estamos colisionando

--- js/mokepon.js
+++ js/mokepon.js
@@ -317,6 +317,12 @@ function pintarCanvas(){
     hipodogeEnemigo.pintarMokepon()
     capipepoEnemigo.pintarMokepon()
     ratigueyaEnemigo.pintarMokepon()
+    if (mascotaJugadorObjeto.velocidadX !== 0 || mascotaJugadorObjeto.velocidadY !== 0){
+        revisarColision(hipodogeEnemigo)
+        revisarColision(capipepoEnemigo)
+        revisarColision(ratigueyaEnemigo)
+
+    }
 }
 
 function moverCapipepoDerecha(){
@@ -366,6 +372,31 @@ function iniciarMapa(){
     intervalo = setInterval(pintarCanvas, 50)
     window.addEventListener('keydown',teclaPresionada)
     window.addEventListener('keyup',detenerMovimiento)
+}
+
+function revisarColision(enemigo){
+    const arribaEnemigo = enemigo.y
+    const abajoEnemigo = enemigo.y + enemigo.alto
+    const derechaEnemigo = enemigo.x + enemigo.ancho
+    const izquierdaEnemigo = enemigo.x
+
+    const arribaMascota = mascotaJugadorObjeto.y
+    const abajoMascota = mascotaJugadorObjeto.y + mascotaJugadorObjeto.alto
+    const derechaMascota = mascotaJugadorObjeto.x + mascotaJugadorObjeto.ancho
+    const izquierdaMascota = mascotaJugadorObjeto.x
+
+    if (
+        abajoMascota < arribaEnemigo || 
+        arribaMascota > abajoEnemigo || 
+        derechaMascota < izquierdaEnemigo || 
+        izquierdaMascota > derechaEnemigo 
+    ) {
+        return
+    }
+    detenerMovimiento()
+    alert("Hay colisión con " + enemigo.nombre)
+    
+
 }
 
 window.addEventListener("load", iniciarJuego)
